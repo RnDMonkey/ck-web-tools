c5c0d01477626f2f7dc976766a63bdc627a8e87c
Ryan Rutledge, CID+ <RnDMonkey@users.noreply.github.com>
2025-11-21 12:05:31 -0800

    Update utils.js

--- web/js/modules/utils.js
+++ web/js/modules/utils.js
@@ -148,13 +148,12 @@ function getDBClosestValue(db, inputColor, colorSpace = "RGB") {
     for (let i = 0; i < db.length; i++) {
         let dbColor = db[i][colorSpace];
 
-        // direct match shortcut
         if (
             dbColor[0] === inputColor[0] &&
             dbColor[1] === inputColor[1] &&
             dbColor[2] === inputColor[2]
         ) {
-            return db[i];
+            return db[i]; //exact match shortcut
         }
 
         let d = distFunc(dbColor, inputColor);
@@ -189,42 +188,6 @@ function trimBrackets(input) {
         str = str.slice(0, -1)
     }
     return str
-}
-
-/* Approach 1) process the entire color db directly
-Pros:
-- no need of remapping values, the return function can also just take in which colorspace to use
-Cons: 
-- Memory constraints? directly manipulating the db here
-
-Approach 2) pass in color dict paired with a guid so we can find it on the db again.
-// pros: data readability, you are only using what you need
-
-Cons:
-- Need to keep track of parity between the passed in list color vs the original color db (can use guids)
-- Takes longer to properly implement lol 
-*/
-// this is approach 1
-function getDBClosestValue(db, inputColor, colorSpace = "RGB"){
-    var closest = {}
-    var dist
-    for (var i = 0; i < db.length; i++) {
-        var dbColor = db[i][colorSpace]// get val from json/db
-        if (dbColor[0] == inputColor[0] && dbColor[1] == inputColor[1] && dbColor[2] == inputColor[2]) { //its the exact same rgb value as a color, just return that
-            return db[i]
-        }
- 
-        dist = Math.pow(dbColor[0] - inputColor[0], 2)
-        dist += Math.pow(dbColor[1] - inputColor[1], 2)
-        dist += Math.pow(dbColor[2] - inputColor[2], 2)
-        // dist = Math.sqrt(dist) // can skip this and use approximate relative distance
-
-        if (!closest.dist || closest.dist > dist) {
-            closest.dist = dist
-            closest.val = db[i]
-        }
-    }
-    return closest.val
 }
 
 // function 
