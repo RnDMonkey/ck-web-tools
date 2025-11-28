139ffc5e863d08a13a8f1f64e81d2f9dc31bcc26
RnDMonkey <ryandrutledge@gmail.com>
2025-11-28 15:34:49 -0800

    changed prettier to include semicolons, change preview table from static to scripted creation

--- web/js/modules/colordb.js
+++ web/js/modules/colordb.js
@@ -1,18 +1,18 @@
-import { trimBrackets } from "./utils.js"
+import { trimBrackets } from "./utils.js";
 
 export async function getColorDB(path = "data/colordb.json") {
-    var data = null
-    const response = await fetch(path)
-    data = await response.json()
+    let data = null;
+    const response = await fetch(path);
+    data = await response.json();
 
     // binding data
     data.forEach((element) => {
         // Map RGB to int array
-        var trimmedRGB = trimBrackets(element["RGB"])
-        element["RGB"] = trimmedRGB.split(",").map(Number)
-    })
+        var trimmedRGB = trimBrackets(element["RGB"]);
+        element["RGB"] = trimmedRGB.split(",").map(Number);
+    });
 
-    console.log("Caching Color DB")
-    console.log(data)
-    return data
+    console.log("Caching Color DB");
+    console.log(data);
+    return data;
 }
--- web/js/modules/globals.js
+++ web/js/modules/globals.js
@@ -55,32 +55,32 @@ export const Globals = {
     overlayCanvasDOM: null,
     overlayCtx: null,
     hoverTooltip: null
-}
+};
 
 export function initGlobals() {
-    Globals.chunkInputX = document.getElementById("chunk-input-x")
-    Globals.chunkInputY = document.getElementById("chunk-input-y")
-    Globals.showGridLinesDOM = document.getElementById("show-grid-lines")
-    Globals.gridThicknessInputDOM = document.getElementById("grid-thickness")
-    Globals.imageUploadDOM = document.getElementById("image-upload")
-    Globals.imgDom = document.getElementById("upload-preview")
-    Globals.previewTableDOM = document.getElementById("preview-table")
-    Globals.uploadedImageDOM = document.getElementById("upload-preview")
-    Globals.gridSizeDOM = document.getElementById("grid-size")
-    Globals.imageInputsDOM = document.getElementById("image-inputs")
-    Globals.itemSelectionsDOM = document.getElementById("item-selections")
-    Globals.itemCountersDOM = document.getElementById("item-counters")
-    Globals.processModeSelectDOM = document.getElementById("process-options")
-    Globals.cam16WeightContainerDOM = document.getElementById("cam16-weight-container")
-    Globals.cam16WeightInputDOM = document.getElementById("cam16-weight")
-    Globals.btnToggleColorsDOM = document.getElementById("btn-toggle-colors")
-    Globals.btnToggleCountersDOM = document.getElementById("btn-toggle-counters")
-    Globals.btnToggleImages = document.getElementById("btn-toggle-images")
-    Globals.btnProcess = document.getElementById("btn-process")
-    Globals.btnRenderPreview = document.getElementById("btn-render-preview")
-    Globals.allowLargerImagesDOM = document.getElementById("allow-larger-images")
-    Globals.outputCanvasDOM = document.getElementById("output-canvas")
-    Globals.overlayCanvasDOM = document.getElementById("overlay-canvas")
-    Globals.overlayCtx = Globals.overlayCanvasDOM.getContext("2d")
-    Globals.hoverTooltip = document.getElementById("hover-tooltip")
+    Globals.chunkInputX = document.getElementById("chunk-input-x");
+    Globals.chunkInputY = document.getElementById("chunk-input-y");
+    Globals.showGridLinesDOM = document.getElementById("show-grid-lines");
+    Globals.gridThicknessInputDOM = document.getElementById("grid-thickness");
+    Globals.imageUploadDOM = document.getElementById("image-upload");
+    Globals.imgDom = document.getElementById("upload-preview");
+    Globals.previewTableDOM = document.getElementById("preview-table");
+    Globals.uploadedImageDOM = document.getElementById("upload-preview");
+    Globals.gridSizeDOM = document.getElementById("grid-size");
+    Globals.imageInputsDOM = document.getElementById("image-inputs");
+    Globals.itemSelectionsDOM = document.getElementById("item-selections");
+    Globals.itemCountersDOM = document.getElementById("item-counters");
+    Globals.processModeSelectDOM = document.getElementById("process-options");
+    Globals.cam16WeightContainerDOM = document.getElementById("cam16-weight-container");
+    Globals.cam16WeightInputDOM = document.getElementById("cam16-weight");
+    Globals.btnToggleColorsDOM = document.getElementById("btn-toggle-colors");
+    Globals.btnToggleCountersDOM = document.getElementById("btn-toggle-counters");
+    Globals.btnToggleImages = document.getElementById("btn-toggle-images");
+    Globals.btnProcess = document.getElementById("btn-process");
+    Globals.btnRenderPreview = document.getElementById("btn-render-preview");
+    Globals.allowLargerImagesDOM = document.getElementById("allow-larger-images");
+    Globals.outputCanvasDOM = document.getElementById("output-canvas");
+    Globals.overlayCanvasDOM = document.getElementById("overlay-canvas");
+    Globals.overlayCtx = Globals.overlayCanvasDOM.getContext("2d");
+    Globals.hoverTooltip = document.getElementById("hover-tooltip");
 }
